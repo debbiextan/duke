9e1de110fc8de9cd418ed6a3039619c15844a323
Debbie Tan <debbietanjm@gmail.com>
2019-08-20 00:48:37 +0800

    Level 3. Mark as Done Without Extension implemented.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,28 +12,47 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n\tWhat can I do for you?");
 
         Scanner sc = new Scanner(System.in);
-        String[] list = new String[100];
+        String[][] list = new String[2][100]; // symbol, list
         String input;
         int n = 0; // list size
 
         while (true) {
             input = sc.nextLine();
-            if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            }
-            else if (input.equals("list")) {
-                // print numbered list
-                for (int i=0; i<n; i++) {
-                    int label = i+1;
-                    System.out.println(label + ". " + list[i]);
-                }
+            String[] DoneCheck = input.split("\\s+");
+            if (DoneCheck[0].equals("done")) {
+                int num = Integer.parseInt(DoneCheck[1]) - 1;
+                list[0][num] = "✓";
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("[✓] " + list[1][num]);
             }
             else {
-                list[n] = input;
-                n++;
-                System.out.println("added: " + list[n - 1]);
+
+                if (input.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                }
+                else if (input.equals("list")) {
+                    System.out.println("Here are the tasks in your list: ");
+                    // print numbered list
+                    for (int i = 0; i < n; i++) {
+                        int label = i + 1;
+                        System.out.println(label + ". [" + list[0][i] + "]" + list[1][i]);
+                    }
+                }
+                else {
+                    list[0][n] = "✗";
+                    list[1][n] = input;
+                    n++;
+                    System.out.println("added: " + list[1][n - 1]);
+                }
             }
         }
+    }
+
+    public String UpdateDone(String icon){
+        if (icon.equals("✗")) {
+            icon = "✓";
+        }
+        return icon;
     }
 }
